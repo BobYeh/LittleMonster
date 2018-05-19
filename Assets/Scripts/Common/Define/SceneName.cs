bf51879f6523064fff5024b7b8031c1188ec16c8
BobYeh <bobgary2002@gmail.com>
2018-05-19 10:29:20 +0900

    Add monster list scene

--- Assets/Scripts/Common/Define/SceneName.cs
+++ Assets/Scripts/Common/Define/SceneName.cs
@@ -7,5 +7,7 @@ public class SceneName
     public static string COMMON = "CommonScene";
     public static string HOME = "HomeScene";
     public static string TITLE = "TitleScene";
-    public static string[] HOME_GROUP = new string[] { HOME };
+    public static string MONSTER_LIST_SCENE= "MonsterListScene";
+    public static string[] HOME_GROUP = new string[] { HOME, MONSTER_LIST_SCENE };
+    public static string[] TITLE_GROUP = new string[] { TITLE};
 }
