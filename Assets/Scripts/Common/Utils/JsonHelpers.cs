23ceb0dbb8e6f779e8d560c9b1584e750c15e70e
BobYeh <bobgary2002@gmail.com>
2018-06-16 11:41:40 +0900

    Add MiniJSON and ResourceUtilities

---
+++ Assets/Scripts/Common/Utils/JsonHelpers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Reflection;
+using System.Linq;
+using MiniJSON;
+
+public class JsonHelpers
+{
+    public static List<T> GetObjectList<T>(string jsonData) where T : new()
+    {
+        var objectArray = (IList<object>)Json.Deserialize(jsonData);
+        List<T> list = new List<T>();
+
+        foreach(var obj in objectArray)
+        {
+            list.Add(DictionaryToObject<T>(obj as Dictionary<string, object>));
+        }
+
+        return list;
+    }
+
+
+    public static T DictionaryToObject<T>(IDictionary<string, object> dict) where T : new()
+    {
+        var t = new T();
+        PropertyInfo[] properties = t.GetType().GetProperties();
+
+        foreach (PropertyInfo property in properties)
+        {
+            if (!dict.Any(x => x.Key.Equals(property.Name, StringComparison.InvariantCultureIgnoreCase)))
+                continue;
+
+            KeyValuePair<string, object> item = dict.First(x => x.Key.Equals(property.Name, StringComparison.InvariantCultureIgnoreCase));
+
+            // Find which property type (int, string, double? etc) the CURRENT property is...
+            Type tPropertyType = t.GetType().GetProperty(property.Name).PropertyType;
+
+            // Fix nullables...
+            Type newT = Nullable.GetUnderlyingType(tPropertyType) ?? tPropertyType;
+
+            // ...and change the type
+            object newA = Convert.ChangeType(item.Value, newT);
+            t.GetType().GetProperty(property.Name).SetValue(t, newA, null);
+        }
+        return t;
+    }
+}
---
+++ Assets/Scripts/Common/Utils/ResourceUtilities.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Common.Utils;
+using UnityEngine.U2D;
+
+public class ResourceUtilities : SingletonMonoBehaviour<ResourceUtilities>
+{
+    public void LoadMonsterIcon(int masterId, Action<Sprite> act)
+    {
+        string atlasPath = "SpriteAtlas/Monster/MonsterIcons";
+        string fileName = "Monster_Icon_" + masterId.ToString("D3");
+
+        var request = Resources.LoadAsync<SpriteAtlas>(atlasPath);
+
+        request.completed +=(async)=> {
+            if (request.asset != null)
+            {
+                SpriteAtlas atlas = (SpriteAtlas)request.asset;
+                Sprite sprite = atlas.GetSprite(fileName);
+                if (sprite != null)
+                {
+                    act(sprite);
+                }
+                else
+                {
+                    Debug.Log("LoadMonsterIcon failed: masterId: " + masterId);
+                }
+            }
+            else
+                Debug.Log("LoadAtals failed: path: " + atlasPath);
+        };
+    }
+}
