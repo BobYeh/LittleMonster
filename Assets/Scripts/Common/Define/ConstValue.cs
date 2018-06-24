088aecf31cba33270c672db4e989a39f1851219e
BobYeh <bobgary2002@gmail.com>
2018-06-24 17:58:38 +0900

    Add ConstValue class

---
+++ Assets/Scripts/Common/Define/ConstValue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConstValue
+{
+    public class PlayerPrefsKey
+    {
+        public const string LAST_VIEWED_PARTYNUMBER = "LastViewedPartyNumber";
+    }
+}
